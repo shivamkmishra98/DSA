d3c107bfd3aa7cfc18b3300d25a1712406f86374
shivam kumar mishra <152583361+shivamkmishra98@users.noreply.github.com>
2026-06-01 00:50:02 +0530

    Time: 19 ms (88.29%) | Memory: 59.4 MB (78.56%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -5,32 +5,3 @@ public:
         return nums[nums.size() - k];     // kth largest
     }
 };
-
-
-
-// 1st step ->First k elements ka group; second ->Ab baaki elements check karenge 3rd ->Group ka smallest element ko print karwa denge 
-
-// class Solution {
-// public:
-//     int findKthLargest(vector<int>& nums, int k) {
-//       int n = nums.size();
-//       for(int i  = k ; i<n; i++){
-//         int minIndex = 0;
-//         for(int j= 1; j <k;j++){
-//             if(nums[j]<nums[minIndex]){
-//                 minIndex  = j;
-//             }
-//         }
-//         if(nums[i]>nums[minIndex]){
-//             nums[minIndex] = nums[i];
-//         }
-//       }
-//       int ans = nums[0];
-//       for(int i = 1; i< k; i++){
-//         if(nums[i]<ans)
-//           ans = nums[i];
-//       }  
-//       return ans;
-//     }
-// };
-   
