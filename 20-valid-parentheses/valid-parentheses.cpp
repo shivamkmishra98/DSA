dd0cabdcde84633c525754e1a9e01788f7e9794c
shivam kumar mishra <152583361+shivamkmishra98@users.noreply.github.com>
2026-07-05 03:24:23 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (37.87%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string str) {
+        stack<char>st;
+
+        for(int i = 0;i<str.size();i++){
+            if(str[i] == '(' || str[i] == '{' || str[i] == '[' ){ //for opening brackets
+                st.push(str[i]); //storing in stack
+            }
+            else{ //for closing brackets 
+                if(st.size() == 0){
+                    return false;
+                }
+                if((st.top() == '(' && str[i] == ')') || (st.top() == '{' && str[i] == '}') || (st.top() == '[' && str[i]== ']')){
+                    st.pop();
+                }
+                else{ // no matches
+                    return false;
+                }
+            }
+        }
+        return st.size() == 0; //for checking in last hamara stack ka value zero hai ya nhi 
+    }
+};
