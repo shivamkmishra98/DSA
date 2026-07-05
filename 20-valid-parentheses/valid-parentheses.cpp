0acf9666e4dcf909ef54dffd91c26cf9e823c0ad
shivam kumar mishra <152583361+shivamkmishra98@users.noreply.github.com>
2026-07-05 21:26:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (67.64%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,3 +1,29 @@
+// class Solution {
+// public:
+//     bool isValid(string str) {
+//      stack<char> st;
+
+//      for(int i =0; i<str.size(); i++){
+//         if(str[i] == '(' || str[i] == '{' || str[i] =='['){
+//             st.push(str[i]);
+//         }
+//         else{
+//             if (st.size() == 0){
+//                 return false;
+//             }
+//             if ((st.pop() == '(' && str[i] == ')'
+//                 st.pop() == '{' && str[i] == '}'
+//                 st.pop() == '[' && str[i] == ']' )){
+//                     st.pop();
+//                 }
+//             else{
+//                 return false;
+//             }
+//         }
+//      } 
+//     return st.size() == 0; 
+//     }
+// };
 class Solution {
 public:
     bool isValid(string str) {
@@ -22,3 +48,6 @@ public:
         return st.size() == 0; //for checking in last hamara stack ka value zero hai ya nhi 
     }
 };
+
+
+
