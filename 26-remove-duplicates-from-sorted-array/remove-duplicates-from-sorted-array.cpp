5c99ddc0b96cdce5ff69b0c9ec3fccc39ea31362
shivam kumar mishra <152583361+shivamkmishra98@users.noreply.github.com>
2026-07-13 01:53:01 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (53.33%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,18 +1,37 @@
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+
+//         int uniqueIndex = 1;
+
+//         for(int currentIndex = 1; currentIndex < nums.size(); currentIndex++) {
+
+//             if(nums[currentIndex] != nums[currentIndex - 1]) {
+
+//                 nums[uniqueIndex] = nums[currentIndex];
+//                 uniqueIndex++;
+//             }
+//         }
+
+//         return uniqueIndex;
+//     }
+// };
+
+
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
 
-        int uniqueIndex = 1;
+        int j = 1;
 
-        for(int currentIndex = 1; currentIndex < nums.size(); currentIndex++) {
-
-            if(nums[currentIndex] != nums[currentIndex - 1]) {
-
-                nums[uniqueIndex] = nums[currentIndex];
-                uniqueIndex++;
+        for(int i = 1; i < nums.size(); i++)
+        {
+            if(nums[i] != nums[j-1])
+            {
+                nums[j++] = nums[i];
             }
         }
 
-        return uniqueIndex;
+        return j;
     }
 };
