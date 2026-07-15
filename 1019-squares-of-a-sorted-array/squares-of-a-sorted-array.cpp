64a3fc5f35058c5f44da9a583eafccda7a9d2025
shivam kumar mishra <152583361+shivamkmishra98@users.noreply.github.com>
2026-07-16 00:46:34 +0530

    Time: 0 ms (100.00%) | Memory: 30.2 MB (67.41%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -51,49 +51,49 @@
 //     }
 // };
 // second by using one exta specifies for position 
-class Solution {
-public:
-    vector<int> sortedSquares(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans(n);
-        int left = 0;
-        int right = n - 1;
-        int pos = n - 1;
-        while (left <= right) {
-            int leftSquare = nums[left] * nums[left];
-            int rightSquare = nums[right] * nums[right];
-
-            if (leftSquare > rightSquare) {
-                ans[pos] = leftSquare;
-                left++;
-            }
-            else {
-                ans[pos] = rightSquare;
-                right--;
-            }
-            pos--;
-        }
-        return ans;
-    }
-};
-// //direct approch of two pointer 
-// class Solution{
+// class Solution {
 // public:
-//     vector<int> sortedSquares(vector<int>& nums){
+//     vector<int> sortedSquares(vector<int>& nums) {
 //         int n = nums.size();
 //         vector<int> ans(n);
 //         int left = 0;
-//         int right = n-1;
-//         for(int i = n-1; i>=0; i--){
-//             if (abs(nums[left] )> abs(nums[right])) {
-//                 ans[i] = nums[left] * nums[left];
+//         int right = n - 1;
+//         int pos = n - 1;
+//         while (left <= right) {
+//             int leftSquare = nums[left] * nums[left];
+//             int rightSquare = nums[right] * nums[right];
+
+//             if (leftSquare > rightSquare) {
+//                 ans[pos] = leftSquare;
 //                 left++;
 //             }
-//             else{
-//                 ans[i] = nums[right] * nums[right];
+//             else {
+//                 ans[pos] = rightSquare;
 //                 right--;
 //             }
+//             pos--;
 //         }
 //         return ans;
 //     }
 // };
+//direct approch of two pointer 
+class Solution{
+public:
+    vector<int> sortedSquares(vector<int>& nums){
+        int n = nums.size();
+        vector<int> ans(n);
+        int left = 0;
+        int right = n-1;
+        for(int i = n-1; i>=0; i--){
+            if (abs(nums[left] )> abs(nums[right])) {
+                ans[i] = nums[left] * nums[left];
+                left++;
+            }
+            else{
+                ans[i] = nums[right] * nums[right];
+                right--;
+            }
+        }
+        return ans;
+    }
+};
