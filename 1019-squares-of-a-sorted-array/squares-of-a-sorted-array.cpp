f5098cd06c7381a17cd1cc04b9e902fa110e4082
shivam kumar mishra <152583361+shivamkmishra98@users.noreply.github.com>
2026-07-16 00:45:57 +0530

    Time: 7 ms (28.54%) | Memory: 30.3 MB (39.83%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,81 +1,81 @@
-// by using merge sort
-class Solution {
-public:
-    vector<int> sortedSquares(vector<int>& nums) {
-
-        vector<int> neg;
-        vector<int> pos;
-
-        for(int x : nums)
-        {
-            if(x < 0)
-                neg.push_back(x * x);
-            else
-                pos.push_back(x * x);
-        }
-
-        reverse(neg.begin(), neg.end());
-
-        vector<int> ans;
-
-        int i = 0;
-        int j = 0;
-
-        while(i < neg.size() && j < pos.size())
-        {
-            if(neg[i] < pos[j])
-            {
-                ans.push_back(neg[i]);
-                i++;
-            }
-            else
-            {
-                ans.push_back(pos[j]);
-                j++;
-            }
-        }
-
-        while(i < neg.size())
-        {
-            ans.push_back(neg[i]);
-            i++;
-        }
-
-        while(j < pos.size())
-        {
-            ans.push_back(pos[j]);
-            j++;
-        }
-
-        return ans;
-    }
-};
-// // second by using one exta specifies for position 
+// // by using merge sort
 // class Solution {
 // public:
 //     vector<int> sortedSquares(vector<int>& nums) {
-//         int n = nums.size();
-//         vector<int> ans(n);
-//         int left = 0;
-//         int right = n - 1;
-//         int pos = n - 1
-//         while (left <= right) {
-//             int leftSquare = nums[left] * nums[left];
-//             int rightSquare = nums[right] * nums[right];
 
-//             if (leftSquare > rightSquare) {
-//                 ans[pos] = leftSquare;
-//                 left++;
-//             }
-//             else {
-//                 ans[pos] = rightSquare;
-//                 right--;
-//             }
-//             pos--;
+//         vector<int> neg;
+//         vector<int> pos;
+
+//         for(int x : nums)
+//         {
+//             if(x < 0)
+//                 neg.push_back(x * x);
+//             else
+//                 pos.push_back(x * x);
 //         }
+
+//         reverse(neg.begin(), neg.end());
+
+//         vector<int> ans;
+
+//         int i = 0;
+//         int j = 0;
+
+//         while(i < neg.size() && j < pos.size())
+//         {
+//             if(neg[i] < pos[j])
+//             {
+//                 ans.push_back(neg[i]);
+//                 i++;
+//             }
+//             else
+//             {
+//                 ans.push_back(pos[j]);
+//                 j++;
+//             }
+//         }
+
+//         while(i < neg.size())
+//         {
+//             ans.push_back(neg[i]);
+//             i++;
+//         }
+
+//         while(j < pos.size())
+//         {
+//             ans.push_back(pos[j]);
+//             j++;
+//         }
+
 //         return ans;
 //     }
 // };
+// second by using one exta specifies for position 
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+        int left = 0;
+        int right = n - 1;
+        int pos = n - 1;
+        while (left <= right) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+
+            if (leftSquare > rightSquare) {
+                ans[pos] = leftSquare;
+                left++;
+            }
+            else {
+                ans[pos] = rightSquare;
+                right--;
+            }
+            pos--;
+        }
+        return ans;
+    }
+};
 // //direct approch of two pointer 
 // class Solution{
 // public:
