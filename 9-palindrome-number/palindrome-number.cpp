7c4c61ca6b89590efc500c86666416c7508dc4da
shivam kumar mishra <shivamkumar2314@lpu.in>
2026-09-04 10:48:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (64.71%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -3,11 +3,11 @@ public:
     bool isPalindrome(int x) {
         if (x < 0) return false;
 
-        long long rev = 0;
+        long long int rev = 0;
         int temp = x;
 
-        while (temp) {
-            rev = rev * 10 + temp % 10;
+        while (temp != 0) {
+             rev = rev * 10 + temp % 10;
             temp /= 10;
         }
 
